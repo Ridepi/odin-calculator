6620f096ba6a24502548a8a66bb9cf83a4478dc3
Rianne <rianne.visser@outlook.com>
2023-03-27 09:38:58 +0200

    Set up calc buttons and basic javascript functions

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function(a, b) {
+    result = a + b;
+    return result
+}
+
+const min = function(a,b) {
+    result = a - b;
+    return result
+}
+
+const div = function(a, b) {
+    result = a/b;
+    return result
+}
+
+const mult = function(a, b) {
+    result = a*b;
+    return result
+}
